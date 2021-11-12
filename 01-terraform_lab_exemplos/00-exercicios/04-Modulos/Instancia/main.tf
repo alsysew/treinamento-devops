96cc617db96054609231b8044293ed570a5d547a
alsysew <andredesanti@gmail.com>
2021-11-12 09:56:36 -0300

    Modulos

--- 01-terraform_lab_exemplos/00-exercicios/04-Modulos/Instancia/main.tf
+++ 01-terraform_lab_exemplos/00-exercicios/04-Modulos/Instancia/main.tf
@@ -1,7 +1,14 @@
+provider "aws" {
+  region = "sa-east-1"
+}
+
+
 resource "aws_instance" "web" {
   ami           = "ami-0856ceeafc1be110c"
   instance_type = "t2.micro"
+  key_name = "Treinamento-dia2-keypair"
   associate_public_ip_address = true
+  subnet_id = "subnet-0f4dc32624c2fb345"
   root_block_device {
     encrypted = true
     volume_size = 8
@@ -10,4 +17,55 @@ resource "aws_instance" "web" {
     Name = "${var.nome}",
     Itau = true
   }
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
 }
+
+
+resource "aws_security_group" "allow_ssh" {
+#  name        = "allow_ssh"
+#  description = "Allow SSH inbound traffic"
+  vpc_id= "vpc-07d2b7ba3dfc13689"
+}
+#      ingress = [
+#        {
+#          description      = "SSH from VPC"
+#          from_port        = 22
+#          to_port          = 22
+#          protocol         = "tcp"
+#          cidr_blocks      = ["0.0.0.0/0"]
+#          ipv6_cidr_blocks = ["::/0"]
+#          prefix_list_ids = null,
+#          security_groups = null,
+#          self            = null
+#        },
+#        {
+#          description      = "SSH from VPC"
+#          from_port        = 80
+#          to_port          = 80
+#          protocol         = "tcp"
+#          cidr_blocks      = ["0.0.0.0/0"]
+#          ipv6_cidr_blocks = ["::/0"]
+#          prefix_list_ids = null,
+#          security_groups = null,
+#          self            = null
+#        }
+#      ]
+#
+#      egress = [
+#        {
+#          from_port        = 0
+#          to_port          = 0
+#          protocol         = "-1"
+#          cidr_blocks      = ["0.0.0.0/0"]
+#          ipv6_cidr_blocks = ["::/0"],
+#          prefix_list_ids  = null,
+#          security_groups  = null,
+#          self             = null,
+#          description      = "Libera dados da rede interna"
+#        }
+#      ]
+
+#      tags = {
+#        Name = "allow_ssh"
+#      }
+#    }
--- 01-terraform_lab_exemplos/00-exercicios/04-Modulos/main.tf
+++ 01-terraform_lab_exemplos/00-exercicios/04-Modulos/main.tf
@@ -1,8 +1,8 @@
-provider "aws" {
-  region = "sa-east-1"
-}
+
 
 module "criar_instancia" {
-  source = "git@github.com:alsysew/Modulo_Terraform.git"
+#  source = "./instancia"
+  source = "git@github.com:alsysew/Modulo_Terraform.git"  
+
   nome = "EC2-Module"
 }
