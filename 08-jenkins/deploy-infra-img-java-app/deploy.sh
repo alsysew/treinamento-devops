cb8c65d1cad6bfcc11a73c354d8ed0d599de381f
Andre Santi <andredesanti@gmail.com>
2021-11-22 14:46:08 -0300

    Jenkins deploy-infra-img-java-app4

--- 08-jenkins/deploy-infra-img-java-app/deploy.sh
+++ 08-jenkins/deploy-infra-img-java-app/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd 08-jenkins/deploy-infra-img-java-app/terraform
+cd /var/lib/jenkins/workspace/PipelineDevSimples/08-jenkins/deploy-infra-img-java-app/terraform
 terraform init
 terraform apply -auto-approve
 
@@ -8,7 +8,7 @@ echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
 
 echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo
-echo "$(/home/ubuntu/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+echo "$(terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
